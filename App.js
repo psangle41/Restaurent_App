4c1f0982283b242f95a6c2c270e13a8f558ff65d
psangle41 <psangle41@gmail.com>
2020-09-01 11:15:58 +0530

    'message'

--- App.js
+++ App.js
@@ -1,34 +1,62 @@
-import { AppLoading } from "expo";
-import * as Font from "expo-font";
-import React, { useState } from "react";
-import { Text, View } from "react-native";
 
-export default function App() {
-  let [fontsLoaded] = useFonts({
-    "Inter-Black": require("./assets/fonts/Inter-Black.otf"),
-    "Inter-SemiBoldItalic":
-      "https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12",
-  });
+import React, { useState, useEffect } from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator, DrawerContent } from '@react-navigation/drawer';
+const Drawer = createDrawerNavigator();
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
+import MainTabScreen from './screens/MainTabScreen'
+import SignInScreen from './screens/SignInScreen'
+import {AuthContext} from './screens/context'
+
+const App = () => {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [userToken, setUserToken] = React.useState(null);
+
+  const authContext = React.useMemo(()=>({
+    signIn:(username, password)=>{
+
+      if(username=='user' && password=='pass'){
+        setUserToken('bnxn');
+        setIsLoading(false);
+
+      }else{
+
+      setUserToken('hg');
+      setIsLoading(false);
+      }
+    },
+  }))
+
+  useEffect(() =>{
+    setTimeout(()=>{
+      setIsLoading(false);
+    },1000);
+
+  }, []);
+
+  if (isLoading){
+    return(
+      <View style={{flex:1, justifyContent:"center", alignItems:'center'}}>
+        <ActivityIndicator size="large"/>
+      </View>
+    )
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Platform Default</Text>
-      <Text style={{ fontFamily: "Inter-Black" }}>Inter Black</Text>
-      <Text style={{ fontFamily: "Inter-SemiBoldItalic" }}>
-        Inter SemiBoldItalic
-      </Text>
-    </View>
+    <AuthContext.Provider value={authContext}>
+    <NavigationContainer>
+      { userToken !== null ?(
+        <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+          <Drawer.Screen name="Home" component={MainTabScreen} />
+        </Drawer.Navigator>
+      ) 
+      :
+      <SignInScreen/>
+      }
+    </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
 
-function useFonts(fontMap) {
-  let [fontsLoaded, setFontsLoaded] = useState(false);
-  (async () => {
-    await Font.loadAsync(fontMap);
-    setFontsLoaded(true);
-  })();
-  return [fontsLoaded];
-}
+export default App;
